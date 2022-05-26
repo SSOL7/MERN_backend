0cc65a59693fd757510b37af35e24c454a042844
Shaho Soltani <33752578+SSOL7@users.noreply.github.com>
2022-05-26 23:12:12 +0300

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,4 @@ module.exports = {
     googleClientSecret: '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',
     mongoURI: 'mongodb+srv://angel:mercuryx@angel.xspia.mongodb.net/?retryWrites=true&w=majority',
     cookieKey: 'angel',
-    SendGridKey: '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',
 };
