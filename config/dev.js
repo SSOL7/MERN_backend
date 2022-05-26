e8925feed6a01cd4153c5ead6edb1706351b9e34
Shaho Soltani <33752578+SSOL7@users.noreply.github.com>
2022-05-26 23:18:39 +0300

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: '491296915177-e591orltn74hgvab3tg6q7pmpbkjaojo.apps.googleusercontent.com',
-    googleClientSecret: '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',
+    googleClientSecret: '',
     mongoURI: '',
     cookieKey: 'angel',
 };
